f661cf450fe5605f8bc0e40eec75890efb728b13
Valentina <valentina.francoa@upb.edu.co>
2022-05-12 01:02:16 -0500

    comentarios receiver

--- receiver.c
+++ receiver.c
@@ -11,7 +11,7 @@ void* enviar(void *arg)
     struct mq_attr attr2;
     attr2.mq_flags = 0;
     attr2.mq_maxmsg = 10;
-    attr2.mq_msgsize = 32;
+    attr2.mq_msgsize = 64;
     attr2.mq_curmsgs = 0;
 
     mqd_t mq2 = mq_open("/mq2", O_WRONLY| O_CREAT, 0644, &attr2);
@@ -30,8 +30,8 @@ void* enviar(void *arg)
 
     mq_close(mq2);
     exit(EXIT_FAILURE);
-
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -40,25 +40,25 @@ int main(int argc, char *argv[])
     struct mq_attr attr;
     attr.mq_flags = 0;
     attr.mq_maxmsg = 10;
-    attr.mq_msgsize = 32;
+    attr.mq_msgsize = 64;
     attr.mq_curmsgs = 0;
 
     mq = mq_open("/mq0", O_RDONLY | O_CREAT, 0644, &attr);
-    char buff[32];
+    char buff[64];
 
     pthread_t threadID;
     pthread_create(&threadID,NULL,&enviar,NULL);
 
     while (1)
     {
-        mq_receive(mq, buff, 32, NULL);
+        mq_receive(mq, buff, 64, NULL);
         printf("Message received: %s\n", buff);
-        if( strncmp(buff, "exit", strlen("exit")) == 0){
-            break;
+        if( strncmp(buff, "exit", strlen("exit")) == 0){ //se comparan dos strings, si el string ingresado por el usurio es exit, se sale del ciclo 
+            break; //strncmp devuelve cero si sus dos string a comparar son iguales
         }
     }
-
+ 
     mq_close(mq);
-    mq_unlink("/mq0");
+    mq_unlink("/mq0"); //remueve el queue de mensajes
     exit(EXIT_SUCCESS);
 }
