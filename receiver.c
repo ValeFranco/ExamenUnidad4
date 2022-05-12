a234fc24ef9cb15d93190fc986cfd6d3232af2c1
JuanMandon <LunarBulletStudios@gmail.com>
2022-05-12 02:01:44 -0500

    Envio mensajes

--- receiver.c
+++ receiver.c
@@ -26,6 +26,21 @@ void* enviar(void *arg)
         {
             break;
         }
+
+        if( strncmp(str, "EnviarMensaje", strlen("EnviarMensaje")) == 0){  //lee txt y lo envia
+            //falta agregar si leer arriba hacia abajo o alrevez
+            FILE *fPointer;
+            fPointer = fopen("MiMensaje.txt", "r");
+            char singleLine[200];
+            while (!feof(fPointer))
+            {
+                fgets(singleLine, 200, fPointer);
+                mq_send(mq2, singleLine, strlen(str) + 1, 0);
+
+            }
+            fclose(fPointer);
+        }
+
     }
 
     mq_close(mq2);
@@ -56,13 +71,12 @@ int main(int argc, char *argv[])
         if( strncmp(buff, "exit", strlen("exit")) == 0){ //se comparan dos strings, si el string ingresado por el usurio es exit, se sale del ciclo 
             break; //strncmp devuelve cero si sus dos string a comparar son iguales
         }
-        if(strncmp(buff, "Enviar Archivo", strlen("Enviar Archivo"))==0)
-        {
 
-        }
+        
+
     }
  
-    mq_close(mq); //cierra la cola
+    mq_close(mq);
     mq_unlink("/mq0"); //remueve el queue de mensajes
     exit(EXIT_SUCCESS);
 }
--- sender.c
+++ sender.c
@@ -41,6 +41,21 @@ int main(int argc, char *argv[])
         {
             break;
         }
+
+        if( strncmp(str, "EnviarMensaje", strlen("EnviarMensaje")) == 0){  //lee txt y lo envia
+            //falta agregar si leer arriba hacia abajo o alrevez
+            FILE *fPointer;
+            fPointer = fopen("MiMensaje.txt", "r");
+            char singleLine[200];
+            while (!feof(fPointer))
+            {
+                fgets(singleLine, 200, fPointer);
+                mq_send(mq, singleLine, strlen(str) + 1, 0);
+
+            }
+            fclose(fPointer);
+        }
+
     }
 
     mq_close(mq);
